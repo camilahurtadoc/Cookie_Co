7e53b109887127a7ca3423350d34d29aa59b03b5
Camila Hurtado <camilahurtadoc@gmail.com>
2026-01-17 12:00:50 -0300

    Hero section in Home page

--- Cookie_Co_front/src/components/HeroSection/HeroSection.jsx
+++ Cookie_Co_front/src/components/HeroSection/HeroSection.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import fotoTienda from '../../assets/images/tienda/2150694732.jpg';
+
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          '& > :not(style)': {
+            my: 2,
+            mx: 10,
+            width: '100%',
+            height: 300,
+          },
+        }}
+      >
+        <Box sx={{ backgroundColor: '#F5E8C7', display: 'flex', p:3}} >
+          <Box  sx={{width:'50%', display:'flex', flexDirection:'column', justifyContent:'center', }} >
+            <Typography variant='h3' component='div' >
+              Cookie Co
+            </Typography>
+            <Typography variant='h6' component='div' gutterBottom>
+              Endulzando la vida
+            </Typography>
+            <Typography variant='body1' component='div' sx={{mt:4}} gutterBottom>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi deleniti aspernatur sint minus accusamus ad ratione similique inventore illo modi nobis officiis.
+            </Typography>
+          </Box>
+          <Box sx={{width:'50%', display:'flex', justifyContent:'center', }}>
+            <img src={fotoTienda} alt='tienda' height='100%'/>
+          </Box>
+        </Box>
+        {/* <Paper elevation={3} /> */}
+      </Box>
+    </>
   )
 }
 
