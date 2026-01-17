e2193c711bd877ea90d0cca2773467a0bbe05246
Camila Hurtado <camilahurtadoc@gmail.com>
2026-01-17 14:50:47 -0300

    highlights spacing fix

--- Cookie_Co_front/src/components/Highlights/Highlights.jsx
+++ Cookie_Co_front/src/components/Highlights/Highlights.jsx
@@ -9,7 +9,7 @@ const Highlights = () => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', my: 2, mx: 10, p:3, backgroundColor: '#5C4033' }}>
       <Typography variant='h4' sx={{m:2, color:'#F5E8C7'}}>¡Los más vendidos!</Typography>
-      <Box sx={{display: 'flex', gap:2 }}>
+      <Box sx={{display: 'flex', justifyContent: 'space-around', gap:2 }}>
         <HighlightsCard alt={'cookie'} img={imageCookie1} title={'Cookie Choc'} price={2990} desc={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac purus nec urna varius faucibus sit amet sed. '} />
         <HighlightsCard alt={'cookie'} img={imageCookie1} title={'Cookie Manjar'} price={3120} desc={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac purus nec urna varius faucibus sit amet sed. '} />
         <HighlightsCard alt={'cookie'} img={imageCookie1} title={'Cookie Limón'} price={3500} desc={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac purus nec urna varius faucibus sit amet sed. '} />
