bff67342b297bf3c016e6d03b6a49bcb23bf931e
Camila Hurtado <camilahurtadoc@gmail.com>
2026-01-23 17:26:38 -0300

    added profile page with context and hooks

--- Cookie_Co_front/src/pages/Profile/Profile.jsx
+++ Cookie_Co_front/src/pages/Profile/Profile.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import Typography from '@mui/material/Typography'
+import React, { useContext, useEffect } from 'react'
+import { UserContext } from '../../context/UserContext'
+import { useNavigate } from 'react-router-dom'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 
 const Profile = () => {
+
+  const { getUserInfo, userEmail } = useContext(UserContext)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    getUserInfo()
+  }, [])
+
   return (
-    <div>Profile</div>
+    <Box sx={{ display: 'flex', flexDirection: 'column', my: 2, mx: 10, p: 3 }}>
+      <Typography variant='h4'>Perfil de Usuario</Typography>
+      <Box sx={{ m: 1 }}>
+        <Typography>Mail: {userEmail}</Typography>
+      </Box>
+      <Box sx={{ m: 1, width: '35ch', display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
+        <Button variant="contained" onClick={() => navigate("/logout")} sx={{ backgroundColor: '#D4A574' }}>
+          Cerrar sesión
+        </Button>
+      </Box>
+
+    </Box>
   )
 }
 
