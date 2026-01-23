b0251564f155dc6b5f55042de6d4414ce37e3828
Camila Hurtado <camilahurtadoc@gmail.com>
2026-01-23 12:05:39 -0300

    added cart functionality with hooks and context, linked to menu and localStorage

--- Cookie_Co_front/src/components/CartCard/CartCard.jsx
+++ Cookie_Co_front/src/components/CartCard/CartCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -10,8 +10,13 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
+import { CartContext } from '../../context/CartContext';
 
-const CartCard = ({ img, alt, title, price }) => {
+const CartCard = ({ img, alt, title, price, count, id }) => {
+
+     const { total, setTotal, cart, setCart,
+        minusCookie2, plusCookie2 } = useContext(CartContext)
+
     return (
         <Card sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Box sx={{ display: 'flex', width: '100%' }}>
@@ -27,28 +32,22 @@ const CartCard = ({ img, alt, title, price }) => {
                         {title}
                     </Typography>
                     <Typography variant="caption" sx={{ color: '#A84A2E', fontWeight: '700' }}>
-                        $ {price}
+                        $ {price.toLocaleString("es-ES", { useGrouping: true })}
                     </Typography>
                 </CardContent>
             </Box>
             <Box sx={{ display: 'flex', alignItems: 'center', m: 2 }}>
                 <CardActions sx={{ display: 'flex', justifyContent: 'center', m: 1 }}>
-                    {/* <Button size="small" variant='contained' sx={{ backgroundColor: '#D4A574', color: '#5C4033' }}>
-                    -
-                    </Button> */}
-                    <IconButton>
+                    <IconButton onClick={() => minusCookie2(price, id)}>
                         <RemoveIcon />
                     </IconButton>
-                    <Typography>1</Typography>
-                    {/* <Button size="small" variant='contained' sx={{ backgroundColor: '#D4A574', color: '#5C4033' }}>
-                    +
-                    </Button> */}
-                    <IconButton>
+                    <Typography>{count}</Typography>
+                    <IconButton onClick={() => plusCookie2(price, id)} >
                         <AddIcon />
                     </IconButton>
                 </CardActions>
                 <Box sx={{ p:2 }}>
-                    <Typography variant='body1' sx={{ color: '#A84A2E', fontWeight: '700', p: 1 }}>${price}</Typography>
+                    <Typography variant='body1' sx={{ color: '#A84A2E', fontWeight: '700', p: 1 }}>${(count*price).toLocaleString("es-ES", { useGrouping: true })}</Typography>
                 </Box>
             </Box>
         </Card>
--- Cookie_Co_front/src/pages/Cart/Cart.jsx
+++ Cookie_Co_front/src/pages/Cart/Cart.jsx
@@ -1,34 +1,83 @@
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import React from 'react'
+import React, { useContext } from 'react'
 import CartCard from '../../components/CartCard/CartCard'
 import cookie_chocolate_amargo_chip_mantequila_mani from '../../assets/images/productos/cookies/chocolate_amargo_chip_mantequila_mani_md.avif'
 import cookie_doble_chip_chocolate from '../../assets/images/productos/cookies/doble_chip_chocolate_md.avif'
 import cookie_vegana_chip_chocolate_nueces from '../../assets/images/productos/cookies/vegana_chip_chocolate_nueces_md.avif'
 import Button from '@mui/material/Button'
+import { CartContext } from '../../context/CartContext'
+import { UserContext } from '../../context/UserContext'
 
 
 const Cart = () => {
+
+  const { total, setTotal, cart, setCart,
+    minusCookie2, plusCookie2 } = useContext(CartContext)
+
+  const { tokenJwt } = useContext(UserContext)
+
+  const sendCart = async () => {
+
+    try {
+      const response = await fetch("http://localhost:5000/api/checkouts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${tokenJwt}`
+        },
+        body: JSON.stringify({ cart })
+      })
+
+      if (!response.ok) {
+        console.log("error al enviar carrito")
+      }
+
+      if (response.ok) {
+        Swal.fire({
+          title: "Pedido Enviado",
+          text: "Su pedido ha sido registrado correctamente.",
+          icon: "success"
+        })
+        setCart([])
+        setTotal(0)
+      }
+
+    } catch (error) {
+      console.log("error: ", error)
+    }
+  }
+
   return (
     <>
       <Box sx={{ display: 'flex', flexDirection: 'column', my: 2, mx: 10, p: 3 }} >
-
         <Typography variant='h5'>Cantidad de items:</Typography>
-        <Box>
-          <Box my={1}>
-            <CartCard img={cookie_chocolate_amargo_chip_mantequila_mani} alt={'galleta chocolate amargo y mantequilla maní'} title={'Cookie Chocolate y Chips de Mantequilla Maní'} price={6000} />
-          </Box>
-          <Box my={1}>
-            <CartCard img={cookie_doble_chip_chocolate} alt={'galleta doble chip chocolate'} title={'Cookie Doble Chip Chocolate'} price={7000} />
-          </Box>
-          <Box my={1}>
-            <CartCard img={cookie_vegana_chip_chocolate_nueces} alt={'galleta vegana chip chocolates y nueves'} title={'Cookie Vegana Chip Chocolate y Nueces'} price={8000} />
-          </Box>
-        </Box>
-        <Box sx={{display:'flex', flexDirection:'column', alignItems:'center', mt:3}}>
 
-        <Typography variant='h5'>Total: $ 21000</Typography>
-        <Button variant='contained' sx={{px:4, backgroundColor:'#A84A2E'}}>Pagar</Button>
+        {
+          cart.map(cookie => (
+            <Box key={cookie.id}>
+              <CartCard img={cookie.img} alt={''} title={cookie.name} price={cookie.price} count={cookie.count} id={cookie.id}/>
+            </Box>
+          ))
+        }
+
+        {/* // <Box>
+        //   <Box my={1}>
+        //     <CartCard img={cookie_chocolate_amargo_chip_mantequila_mani} alt={'galleta chocolate amargo y mantequilla maní'} title={'Cookie Chocolate y Chips de Mantequilla Maní'} price={6000} />
+        //   </Box>
+        //   <Box my={1}>
+        //     <CartCard img={cookie_doble_chip_chocolate} alt={'galleta doble chip chocolate'} title={'Cookie Doble Chip Chocolate'} price={7000} />
+        //   </Box>
+        //   <Box my={1}>
+        //     <CartCard img={cookie_vegana_chip_chocolate_nueces} alt={'galleta vegana chip chocolates y nueves'} title={'Cookie Vegana Chip Chocolate y Nueces'} price={8000} />
+        //   </Box>
+        // </Box> */}
+        {
+          total === 0 ? <Typography variant='h6' align='center'>Tu carrito está vacío.</Typography> : null
+        }
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 3 }}>
+          <Typography variant='h5'>Total: ${total.toLocaleString("es-ES", { useGrouping: true })}</Typography>
+          <Button disabled={!tokenJwt} onClick={sendCart} variant='contained' sx={{ px: 4, backgroundColor: '#A84A2E' }}>Pagar</Button>
         </Box>
       </Box>
     </>
