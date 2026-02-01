beb42868aebeca691d5c6f74549eeed52ad1c2a9
Camila Hurtado <camilahurtadoc@gmail.com>
2026-02-01 11:52:21 -0300

    fixed set function in cookieId context

--- src/context/AxiosCookieIdContext.jsx
+++ src/context/AxiosCookieIdContext.jsx
@@ -9,7 +9,7 @@ const AxiosCookieIdProvider = ({ children }) => {
     async function getCookie(cookieId) {
         try {
             const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/productos/${cookieId}`);
-            setPizza(data)
+            setCookie(data)
         } catch (error) {
             console.error(error);
         }
