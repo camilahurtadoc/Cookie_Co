9caa0a382afbc5e01957f568637d776993e4463a
Camila Hurtado <camilahurtadoc@gmail.com>
2026-02-07 18:36:41 -0300

    fixed menu for mobile view

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -41,37 +41,18 @@ const Menu = () => {
         desc={'Pide aquí las galletas que pide tu corazón. Hechas con amor y dedicación, listas para llevar y disfrutar en la comodidad de tu casa.'}
         img={fotoTienda}
       />
-      <Box sx={{ display: 'flex', flexDirection: 'column', my: 2, mx: 10, p: 3 }}>
-        <Typography variant='h4' sx={{ m: 2, color: 'primary' }}>Nuestros productos</Typography>
-        <Grid container spacing={2} >
+      <Box sx={{ display: 'flex', flexDirection: 'column', my: 2, p: 3 }}>
+        <Grid container spacing={2} sx={{ px: { xs: 2, md: 15 } }} >
+          <Grid size={12}>
+            <Typography variant='h4' sx={{ m: 2, color: 'primary' }}>Nuestros productos</Typography>
+          </Grid>
           {
             listaCookies.map(item => (
-              <Grid key={item.id} size={{ xs: 12, md: 6, lg:4 }}>
+              <Grid key={item.id} size={{ xs: 12, md: 6, lg: 4 }}>
                 <MenuCards img={item.imagen_url} alt={'cookie'} title={item.name} price={item.precio} id={item.id} />
               </Grid>
             ))
           }
-          {/* <Grid size={{ xs: 12, md: 4 }}>
-            <MenuCards img={cookie_blanco_negro} alt={'galleta chocolate blanco y negro'} title={'Cookie Cholocale Blanco y Negro'} price={2000} id={2} />
-          </Grid>
-          <Grid size={{ xs: 12, md: 4 }}>
-            <MenuCards img={cookie_cafe_toffee} alt={'galleta café y toffee'} title={'Cookie Café y Toffee'} price={3000} id={3} />
-          </Grid>
-          <Grid size={{ xs: 12, md: 4 }}>
-            <MenuCards img={cookie_caramelo_coco} alt={'galleta caramelo y coco'} title={'Cookie Caramelo y Coco'} price={4000} id={4} />
-          </Grid>
-          <Grid size={{ xs: 12, md: 4 }}>
-            <MenuCards img={cookie_chocolate_amargo_chip} alt={'galleta chocolate amargo y chips'} title={'Cookie Chocolate Amargo y Chips'} price={5000} id={5} />
-          </Grid>
-          <Grid size={{ xs: 12, md: 4 }}>
-            <MenuCards img={cookie_chocolate_amargo_chip_mantequila_mani} alt={'galleta chocolate amargo y mantequilla maní'} title={'Cookie Chocolate y Chips de Mantequilla Maní'} price={6000} id={6} />
-          </Grid>
-          <Grid size={{ xs: 12, md: 4 }}>
-            <MenuCards img={cookie_doble_chip_chocolate} alt={'galleta doble chip chocolate'} title={'Cookie Doble Chip Chocolate'} price={7000} id={7} />
-          </Grid>
-          <Grid size={{ xs: 12, md: 4 }}>
-            <MenuCards img={cookie_vegana_chip_chocolate_nueces} alt={'galleta vegana chip chocolates y nueves'} title={'Cookie Vegana Chip Chocolate y Nueces'} price={8000} id={8} />
-          </Grid> */}
         </Grid>
       </Box>
     </>
