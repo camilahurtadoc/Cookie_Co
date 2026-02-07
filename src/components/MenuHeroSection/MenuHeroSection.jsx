61e18af38ff5ee4186d0c2d62811486fbcd53bbb
Camila Hurtado <camilahurtadoc@gmail.com>
2026-02-07 18:31:43 -0300

    added mobile view for menu hero section

--- src/components/MenuHeroSection/MenuHeroSection.jsx
+++ src/components/MenuHeroSection/MenuHeroSection.jsx
@@ -1,37 +1,31 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 const MenuHeroSection = ({ title, desc, img }) => {
   return (
     <>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          '& > :not(style)': {
-            my: 2,
-            mx: 10,
-            width: '100%',
-            height: 300,
-          },
-        }}
-      >
-        <Box sx={{ display: 'flex', p: 3 }} >
-          <Box sx={{ width: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
-            <Typography variant='h3' component='div' >
-              {title}
-            </Typography>
-            <Typography variant='h6' component='div' sx={{ mt: 4, textAlign:'center' }} gutterBottom>
-              {desc}
-            </Typography>
-          </Box>
-          <Box sx={{ width: '50%', display: 'flex', justifyContent: 'center', }}>
-            <Box sx={{borderRadius:'16px', overflow:'hidden'}}>
-              <img src={img} alt='tienda' height='100%' />
+      <Box sx={{ backgroundColor: '#EDE4D9', display: 'flex', py: 5, width: '100%', maxWidth: '100vw' }}>
+        <Grid container spacing={2} alignItems={'center'} sx={{ px: { xs: 2, md: 15 } }} >
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+              <Typography variant='h3' component='div' >
+                {title}
+              </Typography>
+              <Typography variant='h6' component='div' sx={{ mt: 4, textAlign: 'center' }} gutterBottom>
+                {desc}
+              </Typography>
             </Box>
-          </Box>
-        </Box>
+          </Grid>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', }}>
+              <Box sx={{ borderRadius: '16px', overflow: 'hidden' }}>
+                <img src={img} alt='tienda' height='100%' width='100%' />
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
     </>
   )
