a4b49d882d0f50ea2d8af4cc99af4f300515c0ac
Camila Hurtado <camilahurtadoc@gmail.com>
2026-02-08 12:18:51 -0300

    linked cookie id to name through context backend

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,18 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { AxiosCookieIdContext } from '../../context/AxiosCookieIdContext'
-import { CartContext } from '../../context/CartContext'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import CardMedia from '@mui/material/CardMedia'
-import CardContent from '@mui/material/CardContent'
 import Card from '@mui/material/Card'
-import CardActions from '@mui/material/CardActions'
-import Button from '@mui/material/Button'
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddIcon from '@mui/icons-material/Add';
 import TableContainer from '@mui/material/TableContainer'
 import Table from '@mui/material/Table'
 import TableHead from '@mui/material/TableHead'
@@ -21,33 +13,20 @@ import TableCell from '@mui/material/TableCell'
 import TableBody from '@mui/material/TableBody'
 import Paper from '@mui/material/Paper'
 import { UserContext } from '../../context/UserContext'
+import { AxiosCookiesContext } from '../../context/AxiosCookiesContext'
 
 const Orders = () => {
 
   const { orderId } = useParams()
-  // console.log('oderIdk:' + orderId)
-
-  // const { cookie, getCookie } = useContext(AxiosCookieIdContext)
 
   const { getUserSpecificOrder, specificOrder } = useContext(UserContext)
+  const { listaCookies, getCookiesList } = useContext(AxiosCookiesContext)
   let count = 0;
-
-  // const { cart, handleclick2,
-  //   minusCookie2, plusCookie2 } = useContext(CartContext)
-
-  // const [cookieInCart, setCookieInCart] = useState(null)
-
 
   useEffect(() => {
     getUserSpecificOrder(orderId)
-    //  console.log('oderId en useEffect:' + orderId)
-    // getCookie(cookieId)
+    getCookiesList()
   }, [])
-
-  useEffect(() => {
-    // const index = cart.findIndex(ThisCookie => ThisCookie.id === cookie.id)
-    // index === -1 ? (setCookieInCart(0)) : (setCookieInCart(cart[index].count))
-  })
 
   return (
     <Box sx={{ display: 'flex', py: 5, width: '100%', maxWidth: '100vw' }}>
@@ -57,15 +36,16 @@ const Orders = () => {
         </Grid>
         <Grid size={12}>
           <Grid size={12}>
-          <Typography variant='h5' sx={{ ml: 2, color: 'primary' }} gutterBottom>Fecha: {specificOrder[0].fecha_creacion ? specificOrder[0].fecha_creacion.substring(0, 10) : ''}</Typography>
-        </Grid>
-        <Grid size={12}></Grid>
+            <Typography variant='h5' sx={{ ml: 2, color: 'primary' }} gutterBottom>Fecha: {specificOrder ? specificOrder[0].fecha_creacion.substring(0, 10) : ''}</Typography>
+          </Grid>
+          <Grid size={12}></Grid>
           <TableContainer component={Paper} >
             <Table aria-label="simple table">
               <TableHead>
                 <TableRow>
                   <TableCell sx={{ fontWeight: 600 }}>Item</TableCell>
-                  <TableCell align="left" sx={{ fontWeight: 600 }}>ID Producto</TableCell>
+                  <TableCell align="left" sx={{ fontWeight: 600 }}>Imagen</TableCell>
+                  <TableCell align="left" sx={{ fontWeight: 600 }}>Producto</TableCell>
                   <TableCell align="left" sx={{ fontWeight: 600 }}>Cantidad</TableCell>
                 </TableRow>
               </TableHead>
@@ -73,7 +53,20 @@ const Orders = () => {
                 {specificOrder ? (specificOrder.map(order => (
                   <TableRow key={order.id}>
                     <TableCell component="th" scope="row" >{++count}</TableCell>
-                    <TableCell align="left">{order.id_producto}</TableCell>
+                    <TableCell align="left">
+                      <Card sx={{ display: 'flex' }}>
+                        <Box sx={{ display: 'flex', width: '100%', pb: 0 }}>
+                          <CardMedia
+                            component="img"
+                            height="160"
+                            image={listaCookies[order.id_producto].imagen_url}
+                            alt={listaCookies[order.id_producto].name}
+                            sx={{minWidth:200, maxWidth:400}}
+                          />
+                        </Box>
+                      </Card>
+                    </TableCell>
+                    <TableCell align="left">{listaCookies[order.id_producto].name}</TableCell>
                     <TableCell align="left">{order.cantidad}</TableCell>
                   </TableRow>
                 ))) : (<TableRow></TableRow>)}
