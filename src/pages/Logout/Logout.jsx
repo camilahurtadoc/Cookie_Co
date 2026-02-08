da045d142a105e37f49fbf8abdc61c9a6a4b08ef
Camila Hurtado <camilahurtadoc@gmail.com>
2026-02-07 22:59:13 -0300

    fixed mobile view and style consistency on logout page

--- src/pages/Logout/Logout.jsx
+++ src/pages/Logout/Logout.jsx
@@ -10,24 +10,31 @@ import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputAdornment from '@mui/material/InputAdornment';
 import { Link } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
 
 const Logout = () => {
 
-      const { logout } = useContext(UserContext)
+  const { logout } = useContext(UserContext)
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', my: 2, mx: 10, p: 3 }}>
-      <Typography variant='h4'>Cerrar Sesión</Typography>
-      <Box sx={{ m: 1 }}>
-        <Typography>¿Estás seguro que quieres cerrar tu sesión?</Typography>
-      </Box>
-      <Box sx={{ m: 1, width: '35ch', display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
-        <Button component={Link} to='/' variant="contained" onClick={logout} sx={{ backgroundColor: '#D4A574' }}>
-          Cerrar sesión
-        </Button>
-      </Box>
-
-    </Box>)
+    <Box sx={{ display: 'flex', flexDirection: 'column', my: 2, p: 3 }}>
+      <Grid container spacing={2} sx={{ px: { xs: 2, md: 15 } }} >
+        <Grid size={12}>
+          <Typography variant='h4' sx={{ m: 2, color: 'primary' }}>Cerrar Sesión</Typography>
+        </Grid>
+        <Grid size={12} sx={{ m: 1 }}>
+          <Typography>¿Estás seguro que quieres cerrar tu sesión?</Typography>
+        </Grid>
+        <Grid size={12}>
+          <Box sx={{ m: 1, width: '35ch', display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
+            <Button component={Link} to='/' variant="contained" onClick={logout} sx={{ backgroundColor: '#D4A574' }}>
+              Cerrar sesión
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
 
 export default Logout
